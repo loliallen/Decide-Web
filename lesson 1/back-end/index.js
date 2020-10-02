8851aa9a831822ec98761691e4515e56a5e51dd2
loliallen <maxim.kasakin@yahoo.com>
2020-10-02 21:24:55 +0300

    fist lection

---
+++ lesson 1/back-end/index.js
@@ -0,0 +1,18 @@
+// обращение к библиотеке
+const express = require('express')
+
+const port = 8000;
+
+// объект приложения express
+const app = express();
+
+app.use('/static', express.static(__dirname + '/public/'))
+
+// роут гет запроса
+app.get('/', (request, response) => { 
+    response.sendFile(__dirname + '/public/index.html')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
---
+++ lesson 1/back-end/public/index.js
@@ -0,0 +1,57 @@
+// types
+// int
+const num = 123
+
+// float/double
+const float = 12.3
+
+// objects *
+    // arrays
+    const array = [
+        '1',
+        1,
+        1.2,
+        {}
+    ]
+
+    // objects
+    const obj = {
+        key: "value"
+    } // <=> const obj = new Object()
+
+
+// strings *
+const str1 = "123"
+str1.length;
+
+const str2 = '123'
+
+// крутые кавчки
+const str3 = `123 ${num}`
+console.log(str3);
+
+// functions
+
+
+// обычные функции
+function foo(){
+    console.log("Hello, my name is foo");
+}
+foo()
+
+// стрелочные функции
+const bar = () => {
+    console.log("Hello, my name is bar");
+}
+bar()
+
+
+// анонимные функции
+
+// не смотреть
+// array.forEach(() => {
+
+// })
+
+
+// спойлер
