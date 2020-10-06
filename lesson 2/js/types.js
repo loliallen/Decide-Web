4beb7df2e5c6cf89c1929e667235f5bd113db645
loliallen <maxim.kasakin@yahoo.com>
2020-10-06 21:56:26 +0300

    l2 done

---
+++ lesson 2/js/types.js
@@ -0,0 +1,13 @@
+var a = null;
+var b = undefined;
+var c = "";
+
+console.log(a)
+console.log(b)
+console.log("\t----")
+console.log(Boolean(a))
+console.log(Boolean(b))
+console.log(Boolean(c))
+console.log("\t----")
+console.log(c == false)
+console.log(c === false)
